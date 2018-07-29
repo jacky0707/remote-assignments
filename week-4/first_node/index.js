f8fa718e7fc1684fdcd52e9ea6af47e48e1561b0
songjie <songjie1120@g.ncu.edu.tw>
2018-07-29 22:14:48 +0800

    assignment2

--- week-4/first_node/index.js
+++ week-4/first_node/index.js
@@ -5,6 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res) {
     res.send('Hello, My Server!' );   
 });
+router.get('/getData', function(req, res) {
+    if(!req.query.number){
+        res.send('Wrong Parameters');   
+    }else{
+        target = parseInt(req.query.number);
+        ans = (target+1)*target/2;
+        res.send('<p>'+ans+'</p>');
+    }
+});
+
 
 app.use(router);
 
